4ebca446c0aeb4fc2e70dafe332578fa9faf21e7
pravendra chauhan <pravendra chauhan>
2016-11-26 12:25:51 +0530

    commit new test class

---
+++ src/test/java/com/automation/spot/testcase/MapDetailsTestCase.java
@@ -0,0 +1,19 @@
+package com.automation.spot.testcase;
+
+import org.testng.annotations.Test;
+
+public class MapDetailsTestCase extends BaseTestCase
+{
+
+	@Test
+	public void loginIntoApp(){
+		try {
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println("In Exception");
+		}
+	}
+
+}
